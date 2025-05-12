c88f0ac2675071834da8238c76930927eddc9be5
Piotr Nowak <125823835+Puegoo@users.noreply.github.com>
2025-05-12 14:36:10 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteImagemin from 'vite-plugin-imagemin'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    viteImagemin({
+      gifsicle: { optimizationLevel: 7 },
+      optipng: { optimizationLevel: 7 },
+      mozjpeg: { quality: 75 },
+      webp: { quality: 75 },
+    })
   ],
+  // Pozyskaj nazwę repozytorium z package.json lub ustaw ją bezpośrednio
+  // Dla GitHub Pages: /{nazwa-repozytorium}/
+  base: '/CV_WebSite/',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    // Generowanie pliku 404.html, który przekieruje na index.html (dla routingu React)
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 })
